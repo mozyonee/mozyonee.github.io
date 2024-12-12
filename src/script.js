c5cd154f125c61da460def911b7e5b69894963f1
mozyonee <leslieleecoleman@gmail.com>
2024-12-12 05:33:59 +0100

    init

---
+++ src/script.js
@@ -0,0 +1,82 @@
+const audioMusic = new Audio('./assets/audio/music/prototypek.mp3');
+const headerBlock = document.getElementById("block-header");
+const headerClose = document.getElementById("close-header");
+const mainBlock = document.getElementById("block-main");
+const mainClose = document.getElementById("close-main");
+const baseTypeSpeed = 300;
+const baseDeleteSpeed = 200;
+const deleteDelay = 2000;
+const text = "macr0n";
+let typing = true;
+let index = 0;
+
+const projects = document.querySelector('.projects');
+const education = document.querySelector('.education');
+const columns = document.querySelectorAll('.column');
+const mediaQuery = window.matchMedia('(max-width: 800px)');
+
+const headings = document.querySelectorAll('h3');
+const audioClick = new Audio('./assets/audio/sfx/click.ogg');
+
+const getRandomSpeed = (baseSpeed) => {
+	const radius = baseSpeed;
+	const randomSpeed = Math.floor(baseSpeed - radius + Math.random() * (2 * radius));
+	return randomSpeed;
+}
+
+const typeText = () => {
+	if (typing) {
+		if (index < text.length) {
+			headerClose.textContent += text[index];
+			index++;
+			setTimeout(typeText, getRandomSpeed(baseTypeSpeed));
+		} else {
+			typing = false;
+			setTimeout(typeText, deleteDelay);
+		}
+	} else {
+		if (index > 0) {
+			headerClose.textContent = text.substring(0, index - 1);
+			index--;
+			setTimeout(typeText, getRandomSpeed(baseDeleteSpeed));
+		} else {
+			typing = true;
+			setTimeout(typeText, getRandomSpeed(baseTypeSpeed));
+		}
+	}
+}
+typeText();
+
+const handleMediaChange = (event) => {
+	if (event.matches) {
+		columns[0].appendChild(projects);
+		columns[1].insertBefore(education, columns[1].firstChild);
+	} else {
+		columns[1].insertBefore(projects, columns[1].firstChild);
+		columns[0].appendChild(education);
+	}
+}
+handleMediaChange(mediaQuery);
+
+mediaQuery.addEventListener('change', handleMediaChange);
+
+headerClose.addEventListener('click', () => {
+	headerBlock.classList.add('fade-out');
+	mainBlock.classList.remove('fade-out');
+	audioMusic.pause();
+});
+
+mainClose.addEventListener('click', () => {
+	headerBlock.classList.remove('fade-out');
+	mainBlock.classList.add('fade-out');
+	audioMusic.loop = true;
+	audioMusic.play();
+})
+
+headings.forEach(heading => {
+	heading.addEventListener('click', () => {
+		const paragraph = heading.parentElement;
+		paragraph.classList.toggle('active');
+		audioClick.play();
+	});
+});
